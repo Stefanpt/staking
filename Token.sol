1030095f83d585d9782bcaf330c5c20c8f6c3d6b
Stefanpt <Stefanpt@users.noreply.github.com>
2022-03-31 14:57:45 +0200

    Updated Token Name

--- Token.sol
+++ Token.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT LICENSE
 
-pragma solidity 0.8.4;
+pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract GracelandRewards is ERC20, ERC20Burnable, Ownable {
+contract ChamberRewards is ERC20, ERC20Burnable, Ownable {
 
     mapping(address => bool) controllers;
 
