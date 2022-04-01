aa29476f68d534ac37a786e232eaee8b135dd83a
Stefanpt <Stefanpt@users.noreply.github.com>
2022-04-01 08:49:11 +0200

    Changed Token contract to standard OZ version

--- Token.sol
+++ Token.sol
@@ -1,36 +1,13 @@
-// SPDX-License-Identifier: MIT LICENSE
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-pragma solidity 0.8.9;
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
 
-contract ChamberRewards is ERC20, ERC20Burnable, Ownable {
-
-    mapping(address => bool) controllers;
-
-    constructor() ERC20("GracelandRewards", "GLR") { }
-
-    function mint(address to, uint256 amount) external {
-        require(controllers[msg.sender], "Only controllers can mint");
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    function burnFrom(address account, uint256 amount) public override {
-        if (controllers[msg.sender]) {
-            _burn(account, amount);
-        }
-        else {
-            super.burnFrom(account, amount);
-        }
-    }
-
-    function addController(address controller) external onlyOwner {
-        controllers[controller] = true;
-    }
-
-    function removeController(address controller) external onlyOwner {
-        controllers[controller] = false;
     }
 }
