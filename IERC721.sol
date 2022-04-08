c159395527ae45ce4576b97bee88ca54f74e873e
Stefanpt <Stefanpt@users.noreply.github.com>
2022-04-08 13:13:11 +0200

    deleted dup function

--- IERC721.sol
+++ IERC721.sol
@@ -9,7 +9,6 @@ interface ERC721 {
 
     function balanceOf(address _owner) external view returns (uint256);
     function ownerOf(uint256 _tokenId) external view returns (address);
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
     function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
     function approve(address _approved, uint256 _tokenId) external payable;
