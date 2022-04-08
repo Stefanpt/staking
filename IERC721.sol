a18a46c99685193eea6827ab8e5678b2b903e119
Stefanpt <Stefanpt@users.noreply.github.com>
2022-04-08 19:45:05 +0200

    erc721 change

--- IERC721.sol
+++ IERC721.sol
@@ -15,9 +15,9 @@ interface ERC721 {
     function setApprovalForAll(address _operator, bool _approved) external;
     function getApproved(uint256 _tokenId) external view returns (address);
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
-    function mint(uint256 _mintAmount) public payable;
-    function mintForAddress(uint256 _mintAmount, address _receiver) public;
-    function walletOfOwner(address _owner) public view returns (uint256[] memory);
-    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory);
-
+    function mint(uint256 _mintAmount) external payable;
+    function mintForAddress(uint256 _mintAmount, address _receiver) external;
+    function walletOfOwner(address _owner) external view returns (uint256[] memory);
+    function tokenURI(uint256 _tokenId) external view returns (string memory);
+    function totalSupply() external view returns (uint256);
 }
